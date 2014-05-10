4ab8433869cadcd232f511aa9ede46b5511626b9
jack <jack2w@sina.cn>
2014-05-10 19:37:24 +0800

    add test

---
+++ src/test/java/com/microcard/client/HttpClientTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.microcard.client;
+
+import junit.framework.TestCase;
+
+/**
+ * @author jack
+ *
+ */
+public class HttpClientTest extends TestCase {
+
+	/**
+	 * @param name
+	 */
+	public HttpClientTest(String name) {
+		super(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
