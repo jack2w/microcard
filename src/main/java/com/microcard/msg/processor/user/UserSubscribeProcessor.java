174913b9269b2690e55471371363645efeb3f24c
jack <jack2w@sina.cn>
2014-05-20 23:53:29 +0800

    comment on UserSubscribeProcessor

--- src/main/java/com/microcard/msg/processor/user/UserSubscribeProcessor.java
+++ src/main/java/com/microcard/msg/processor/user/UserSubscribeProcessor.java
@@ -17,7 +17,11 @@ public class UserSubscribeProcessor implements IMsgProcessor {
 	 */
 	@Override
 	public String proccess(Msg msg) throws Exception {
-		// TODO Auto-generated method stub
+		// TODO 当用户扫描一个带参数的二维码时，ReceivedSubscribeMsg的eventKey有值
+		// TODO 当用户扫描一个不带参数的二维码时，ReceivedSubscribeMsg的eventKey无值
+		// TODO eventKey的格式是qrscene_123123,qrscene是前缀，123123是Shop的id
+		// TODO 如果eventKey无值，则检查该User是否存在，不存在则增加记录，如果存在则修改deleteFlag为false，并更新用户的信息
+		// TODO 如果eventKey有值，做无值时的用户更新，并则根据qrscene_123123的Shop id找到Shop，建立与该Shop的关系，注意不要重复建立
 		return null;
 	}
 
--- src/main/java/com/microcard/msg/processor/user/UserUnsubscribeProcessor.java
+++ src/main/java/com/microcard/msg/processor/user/UserUnsubscribeProcessor.java
@@ -17,7 +17,8 @@ public class UserUnsubscribeProcessor  implements IMsgProcessor {
 	 */
 	@Override
 	public String proccess(Msg msg) throws Exception {
-		// TODO Auto-generated method stub
+		// TODO 根据ReceivedUnscribeMsg中fromUserName对应User的openid找到User
+		// TODO 如果存在这个User，然后把这个User设置逻辑删除，如果不存在不处理，日志中记录warn日志，说明取消订阅不应该存在找不到的用户
 		return null;
 	}
 
